3299fdecf14ca4d81f3fa10c1f881d2507e5e515
mairagama <156704587+mairagama@users.noreply.github.com>
2026-01-05 21:22:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual seu nome?");
+let nota1 = prompt("Digite a Nota 1:");
+let nota2 = prompt("Digite a Nota 2:");
+const mediaPonderada = ((nota1*4) + (nota2*6))/10;
+alert("Olá " + nome + ", sua média ponderada é: " + mediaPonderada + "!");
